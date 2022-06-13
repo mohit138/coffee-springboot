e6ed9992a79e08901b1bf56ba70fcf3528f006ea
Mohit Gidwani <mohit22gidwani@gmail.com>
2022-06-13 16:06:02 +0530

    adding mongo db atlas to BE

--- src/main/java/com/store/coffee/Repository/ProductRepository.java
+++ src/main/java/com/store/coffee/Repository/ProductRepository.java
@@ -1,9 +1,19 @@
 package com.store.coffee.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.store.coffee.models.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
+public interface ProductRepository extends MongoRepository<Product, Long> {
+	@Query("{name:'?0'}")
+    Product findItemByName(String name);
+    
+    @Query(value="{category:'?0'}", fields="{'name' : 1, 'quantity' : 1}")
+    List<Product> findAll(String category);
+    
+    public long count();
 }
--- src/main/java/com/store/coffee/models/Product.java
+++ src/main/java/com/store/coffee/models/Product.java
@@ -1,25 +1,30 @@
 package com.store.coffee.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import org.springframework.data.annotation.Id;
 
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.EntityListeners;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
 
-@Entity
-@Table(name="products")
-@EntityListeners(AuditingEntityListener.class)
+//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+//@Entity
+//@Table(name="products")
+//@EntityListeners(AuditingEntityListener.class)
+@Document(collection="products")
 public class Product {
 	
 	public Product() {
 		
 	}
 	
-	public Product(String name, String flavour, String grind, String type, String description, long price) {
+	public Product(long id,String name, String flavour, String grind, String type, String description, long price) {
+		this.id = id;
 		this.name = name;
 		this.flavour = flavour;
 		this.grind = grind;
@@ -29,25 +34,25 @@ public class Product {
 	}
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+//	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
-	@Column(name="product_name", nullable=false)
+//	@Column(name="product_name", nullable=false)
 	private String name;
 	
-	@Column(name="product_flavour",nullable=false)
+//	@Column(name="product_flavour",nullable=false)
 	private String flavour;
 	
-	@Column(name="product_grind", nullable=false)
+//	@Column(name="product_grind", nullable=false)
 	private String grind;
 	
-	@Column(name="product_type",nullable=false)
+//	@Column(name="product_type",nullable=false)
 	private String type;
 	
-	@Column(name="product_description", nullable=false)
+//	@Column(name="product_description", nullable=false)
 	private String description;
 	
-	@Column(name="product_price",nullable=false)
+//	@Column(name="product_price",nullable=false)
 	private long price;
 
 	public long getId() {
